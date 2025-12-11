14cf6c49a6798538cdd8a684d76e17447528d6a7
gok5959 <dngnsooo@naver.com>
2025-12-11 11:22:09 +0900

    feat: 유저 도메인 DTO 수정 - Lombok 어노테이션 추가

--- src/main/java/com/mycom/myapp/domain/user/dto/UserSearchCondition.java
+++ src/main/java/com/mycom/myapp/domain/user/dto/UserSearchCondition.java
@@ -1,7 +1,11 @@
 package com.mycom.myapp.domain.user.dto;
 
 import com.mycom.myapp.domain.user.UserRole;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class UserSearchCondition {
     private String keyword;
     private UserRole role;
