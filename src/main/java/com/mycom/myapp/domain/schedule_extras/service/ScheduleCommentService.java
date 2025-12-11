d9c03a2dbe1abff230e3dae61f4f9bd14ce25c0a
Ujin28 <melon4092@naver.com>
2025-12-11 17:57:25 +0900

    feat: schedule_extras service 구현

--- src/main/java/com/mycom/myapp/domain/schedule_extras/service/ScheduleCommentService.java
+++ src/main/java/com/mycom/myapp/domain/schedule_extras/service/ScheduleCommentService.java
@@ -6,13 +6,6 @@ import com.mycom.myapp.domain.schedule_extras.dto.ScheduleCommentResponse;
 import java.util.List;
 
 public interface ScheduleCommentService {
-//    ScheduleCommentResponse createComment(Long scheduleId, ScheduleCommentCreateRequest request);
-//
-//    List<ScheduleCommentResponse> getCommentBySchedule(Long scheduleId);
-//
-//    ScheduleCommentResponse updateComment(Long commentId, String newContent);
-//
-//    void deleteComment(Long commentId);
 
     ScheduleCommentResponse createComment(Long scheduleId, Long userId, String content);
 
