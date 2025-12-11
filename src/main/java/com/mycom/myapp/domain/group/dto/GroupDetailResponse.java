347a5e1ee87ad1f21fe3ee8e8fd2b8287a55076e
gok5959 <dngnsooo@naver.com>
2025-12-11 17:14:26 +0900

    test: Group 도메인 DTO 수정 - 검색 조건 role -> keyword 변경 - 로직 구현에 필요한 어노테이션, 팩토리메서드 추가

--- src/main/java/com/mycom/myapp/domain/group/dto/GroupDetailResponse.java
+++ src/main/java/com/mycom/myapp/domain/group/dto/GroupDetailResponse.java
@@ -1,11 +1,18 @@
 package com.mycom.myapp.domain.group.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mycom.myapp.domain.group.GroupMemberRole;
+import com.mycom.myapp.domain.group.entity.Group;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GroupDetailResponse {
     private Long id;
     private String name;
@@ -13,5 +20,27 @@ public class GroupDetailResponse {
     private Long ownerId;
     private String ownerName;
     private GroupMemberRole myRole;
-    private int memberCount;
+    private long memberCount;
+
+    public static GroupDetailResponse from(Group group) {
+        return GroupDetailResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .ownerId(group.getOwner().getId())
+                .ownerName(group.getOwner().getName())
+                .build();
+    }
+
+    public static GroupDetailResponse from(Group group, GroupMemberRole myRole, long memberCount) {
+        return GroupDetailResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .ownerId(group.getOwner().getId())
+                .ownerName(group.getOwner().getName())
+                .myRole(myRole)
+                .memberCount(memberCount)
+                .build();
+    }
 }
--- src/main/java/com/mycom/myapp/domain/group/dto/GroupListResponse.java
+++ src/main/java/com/mycom/myapp/domain/group/dto/GroupListResponse.java
@@ -1,15 +1,39 @@
 package com.mycom.myapp.domain.group.dto;
 
 import com.mycom.myapp.domain.group.GroupMemberRole;
+import com.mycom.myapp.domain.group.entity.Group;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class GroupListResponse {
     private Long id;
     private String name;
     private String description;
     private Long ownerId;
+    private String ownerName;
     private GroupMemberRole myRole;
+
+    public static GroupListResponse from(Group group) {
+        return GroupListResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .ownerId(group.getOwner().getId())
+                .ownerName(group.getOwner().getName())
+                .build();
+    }
+
+    public static GroupListResponse from(Group group, GroupMemberRole myRole) {
+        return GroupListResponse.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .ownerId(group.getOwner().getId())
+                .myRole(myRole)
+                .build();
+    }
 }
